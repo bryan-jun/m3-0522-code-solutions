5848899d31e0c351cafc5f6625c5485c74ba8653
Bryan Jun <bryjun@gmail.com>
2022-05-11 04:27:20 +0000

    completed es6 classes

---
+++ es6-classes/student.js
@@ -0,0 +1,24 @@
+/* exported Student */
+
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getIntroduction() {
+    return `Hello, my name is ${this.getFullName()} and I am studying ${this.subject}`;
+  }
+
+}
+
+Student.prototype.getIntroduction = function () {
+  const { subject } = this;
+  const fullName = this.getFullName();
+  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+};
