bdfd2abcb29dfb05f9ceeeb0f2122dc43ca18218
Bryan Jun <bryjun@gmail.com>
2022-05-18 04:22:04 +0000

    completed react-function-components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function CustomButton() {
+  return <button>Click Me</button>;
+}
+
+const element = CustomButton();
+
+const container = document.querySelector('#root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
