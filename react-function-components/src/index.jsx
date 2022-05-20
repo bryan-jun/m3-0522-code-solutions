cdff8752179ce5c26bea8a79738b0a393aa28c87
Bryan Jun <bryjun@gmail.com>
2022-05-20 03:49:20 +0000

    updated react function components

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -5,7 +5,7 @@ function CustomButton() {
   return <button>Click Me</button>;
 }
 
-const element = CustomButton();
+const element = <CustomButton />;
 
 const container = document.querySelector('#root');
 
