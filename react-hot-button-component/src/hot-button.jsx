e3867b1c93288db6eedd361046aa94f1b7a9237e
Bryan Jun <bryjun@gmail.com>
2022-05-27 02:04:23 +0000

    made changes to hot button

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -4,46 +4,35 @@ export default class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { count: 0, isClicked: false };
+    this.state = { count: 0 };
   }
 
   handleClick() {
     const count = this.state.count;
     this.setState({ count: count + 1 });
-    this.setState({ isClicked: true });
   }
 
   render() {
-    let button;
+    let btnClass;
     const count = this.state.count;
-    const isClicked = this.state.isClicked;
 
-    if (isClicked) {
-
-      if (count < 3) {
-        button = <button onClick={this.handleClick}>Hot Button</button>;
-      } else if (count >= 3 && count < 6) {
-        button = <button className="purple" onClick={this.handleClick}>Hot Button</button>;
-      } else if (count >= 6 && count < 9) {
-        button = <button className="violet" onClick={this.handleClick}>Hot Button</button>;
-
-      } else if (count >= 9 && count < 12) {
-        button = <button className="red" onClick={this.handleClick}>Hot Button</button>;
-
-      } else if (count >= 12 && count < 15) {
-        button = <button className="orange" onClick={this.handleClick}>Hot Button</button>;
-      } else if (count >= 15 && count < 18) {
-        button = <button className="yellow" onClick={this.handleClick}>Hot Button</button>;
-      } else if (count >= 18) {
-        button = <button className="white" onClick={this.handleClick}>Hot Button</button>;
-      }
-    } else {
-      button = <button onClick={this.handleClick}>Hot Button</button>;
+    if (count < 3) {
+      btnClass = '';
+    } else if (count >= 3 && count < 6) {
+      btnClass = 'purple';
+    } else if (count >= 6 && count < 9) {
+      btnClass = 'violet';
+    } else if (count >= 9 && count < 12) {
+      btnClass = 'red';
+    } else if (count >= 12 && count < 15) {
+      btnClass = 'orange';
+    } else if (count >= 15 && count < 18) {
+      btnClass = 'yellow';
+    } else if (count >= 18) {
+      btnClass = 'white';
     }
-    return (
-    <div>
-    {button}
-    </div>
-    );
+
+    return <button className={btnClass} onClick = {this.handleClick}>Hot Button</button>;
+
   }
 }
