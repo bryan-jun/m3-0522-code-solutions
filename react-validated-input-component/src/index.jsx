79859564fd97f6cf4269bcdb2cdf500cea915e24
Bryan Jun <bryjun@gmail.com>
2022-06-03 00:23:32 +0000

    completed react validated input component

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ValidatedInput from './validated-input';
+
+const element = <ValidatedInput />;
+
+const container = document.querySelector('#root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
