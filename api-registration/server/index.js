507e81b53a3d525d38a5ba15a071fb2db1064443
Bryan Jun <bryjun@gmail.com>
2022-06-17 02:51:50 +0000

    updated to remove comments

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -1,11 +1,11 @@
 require('dotenv/config');
 const pg = require('pg');
-const argon2 = require('argon2'); // eslint-disable-line
+const argon2 = require('argon2');
 const express = require('express');
 const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 
-const db = new pg.Pool({ // eslint-disable-line
+const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
@@ -43,18 +43,6 @@ app.post('/api/auth/sign-up', (req, res, next) => {
     }
 
     ).catch(err => next(err));
-
-  /**
-   * Hash the user's password with `argon2.hash()`
-   * Then, 😉
-   *   Insert the user's "username" and "hashedPassword" into the "users" table.
-   *   Then, 😉
-   *     Respond to the client with a 201 status code and the new user's "userId", "username", and "createdAt" timestamp.
-   *   Catch any errors.
-   * Catch any errors.
-   *
-   * Hint: Insert statements can include a `returning` clause to retrieve the insterted row(s).
-   */
 
 });
 
